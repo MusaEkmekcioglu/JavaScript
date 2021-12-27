b207747f9f97a7f714fcba206f1ba9417150b7d6
Musa Ekmekcioglu <81248368+MusaEkmekcioglu@users.noreply.github.com>
2021-12-27 15:09:31 +0200

    Add files via upload

---
+++ hackyoufuture/app.js
@@ -0,0 +1,48 @@
+const item = [
+    {
+      
+     productId:1,
+     title: "REVET LONG SLEEVE JERSEY",
+     price: "€165,00",
+     description:"A visible, warm garment for long rides where versatility is paramount.",
+     img:"https://i8.amplience.net/i/rapha/18-11-02-Rapha-Studio-154?aspect=16:9&sm=aspect&scaleFit=poi&poi={($this.metadata.pointOfInterest.w==0)?0.5:$this.metadata.pointOfInterest.x},{($this.metadata.pointOfInterest.w==0)?0.5:$this.metadata.pointOfInterest.y},{($this.metadata.pointOfInterest.w==0)?0:$this.metadata.pointOfInterest.w},{($this.metadata.pointOfInterest.h==0)?0:$this.metadata.pointOfInterest.h}}&fmt.jpeg.interlaced=true&w=2000",
+},
+{  
+    productId:2,
+    title: "MEN'S CORE JERSEY",
+    price: "€70,00",
+    description:"A versatile mid-weight cycling jersey made for comfort on any ride.",
+    img:"https://i8.amplience.net/i/rapha/CoreCollection_TriptychSet_Product_H1-19_01?aspect=16:9&sm=aspect&scaleFit=poi&poi={($this.metadata.pointOfInterest.w==0)?0.5:$this.metadata.pointOfInterest.x},{($this.metadata.pointOfInterest.w==0)?0.5:$this.metadata.pointOfInterest.y},{($this.metadata.pointOfInterest.w==0)?0:$this.metadata.pointOfInterest.w},{($this.metadata.pointOfInterest.h==0)?0:$this.metadata.pointOfInterest.h}}&fmt.jpeg.interlaced=true&w=2000",
+
+},
+    {productId:3,
+    title:"CARGO BIB SHORTS",
+    price: "€230,00",
+    description:"A pair of bib shorts featuring easy-access leg pockets and made with lightweight, extremely water-resistant material.",
+    img:"https://i8.amplience.net/i/rapha/H1-21_Cargo-Bibshorts-On-Model-23-Final?aspect=16:9&sm=aspect&scaleFit=poi&poi={($this.metadata.pointOfInterest.w==0)?0.5:$this.metadata.pointOfInterest.x},{($this.metadata.pointOfInterest.w==0)?0.5:$this.metadata.pointOfInterest.y},{($this.metadata.pointOfInterest.w==0)?0:$this.metadata.pointOfInterest.w},{($this.metadata.pointOfInterest.h==0)?0:$this.metadata.pointOfInterest.h}}&fmt.jpeg.interlaced=true&w=2000",
+
+
+
+},
+{productId:4,
+    title:"MEN'S BREVET JERSEY",
+    price: "€155,00",
+    description:"An evolution of Rapha's practical, comfortable and reflective long-distance jersey.",
+    img:"https://i8.amplience.net/i/rapha/H1-21_Brevet-Jersey-On-Model-12-Final?aspect=16:9&sm=aspect&scaleFit=poi&poi={($this.metadata.pointOfInterest.w==0)?0.5:$this.metadata.pointOfInterest.x},{($this.metadata.pointOfInterest.w==0)?0.5:$this.metadata.pointOfInterest.y},{($this.metadata.pointOfInterest.w==0)?0:$this.metadata.pointOfInterest.w},{($this.metadata.pointOfInterest.h==0)?0:$this.metadata.pointOfInterest.h}}&fmt.jpeg.interlaced=true&w=2000",
+
+
+
+},
+{productId:5,
+    title:"MEN'S LONG SLEEVE CORE JERSEY",
+    price: "€85,00",
+    description:"A long sleeve jersey for riding in cooler climes, made with carefully selected materials and technology to set the new standard in performance, comfort and value.",
+    img:"https://i8.amplience.net/i/rapha/H1-21_Core-Long-Sleeve-Jersey-On-Model-9-Final?aspect=16:9&sm=aspect&scaleFit=poi&poi={($this.metadata.pointOfInterest.w==0)?0.5:$this.metadata.pointOfInterest.x},{($this.metadata.pointOfInterest.w==0)?0.5:$this.metadata.pointOfInterest.y},{($this.metadata.pointOfInterest.w==0)?0:$this.metadata.pointOfInterest.w},{($this.metadata.pointOfInterest.h==0)?0:$this.metadata.pointOfInterest.h}}&fmt.jpeg.interlaced=true&w=2000",
+
+
+
+}
+]
+
+//https://www.rapha.cc/nl/en/on-bike/category/On-Bike-Mens 
+
